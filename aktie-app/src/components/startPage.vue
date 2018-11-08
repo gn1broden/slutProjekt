b946d7862698fa20260baa0e5cca80af1ce90f65
gn1broden <gustav@ebelingwebb.se>
2018-11-08 19:40:22 +0100

    test
    test öl api

---
+++ aktie-app/src/components/startPage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="startPage">
+        <h1>{{msg}}</h1>
+        <section class="sektionOne">
+            <p>Stock test</p>
+            <stockTest />
+        </section>
+        <section class="sektionTwo">
+            <p>sektion 2</p>
+        </section>
+        <section class="sektionTree">
+            <p>sektion 3</p>
+        </section>
+    </div>
+</template>
+
+
+
+<script>
+import stockTest from '@/components/stockTest.vue'
+
+export default {
+  name: 'startPage',
+  components:{
+      stockTest
+  },
+  props: {
+    msg: String
+  }
+  
+}
+</script>
---
+++ aktie-app/src/components/stockTest.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="stockTest">
+        <p>{{crawl}}</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: "stockTest",
+  data() {
+    return {
+      url: "http://sandbox-api.brewerydb.com/v2/beers?key=a8079a799453779042699fa42859cec9",
+      crawl: "Loading..."
+    };
+  },
+  created: function() {
+    this.loadStarWars();
+  },
+  methods: {
+    loadStarWars() {
+      this.$http
+      //console.log(this.url)
+        .get(this.url)
+        
+        .then(resp => {
+          console.log(resp.data.id[0])
+          //this.crawl = resp.data.id[0];
+        })
+        .catch(err => console.log(err));
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
